fff0e44f00abc0c3e4047186095851122137e69c
VictorMundia <victor.mundia@strathmore.edu>
2024-07-01 16:45:48 +0300

    New additions

--- cart.php
+++ cart.php
@@ -20,7 +20,7 @@ if(isset($_POST['add_to_cart'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="Styles/index.css">
+  <link rel="stylesheet" href="">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <title>Shopping Cart</title>
 </head>
@@ -29,7 +29,7 @@ if(isset($_POST['add_to_cart'])) {
       <!-- Include your navbar here -->
       <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
       <div class="container">
-          <a class="navbar-brand" href="main/index.php"><img src="Images/EventHubpng/logo2.png" alt="logo" width="250px" height="50px"></a>
+          <a class="navbar-brand" href="index.php"><img src="Images/EventHubpng/logo2.png" alt="logo" width="250px" height="50px"></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -40,7 +40,7 @@ if(isset($_POST['add_to_cart'])) {
     <button class="btn btn-outline-success" type="submit">Search</button>
   </form>
   <li class="nav-item active">
-    <a class="nav-link " aria-current="page" href="#">Home</a>
+    <a class="nav-link " aria-current="page" href="index.php">Home</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="main/aboutus.php">Browse</a>
